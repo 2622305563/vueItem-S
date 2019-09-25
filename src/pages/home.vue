b37bf0a1419528396810e23d241a2ef754e7f5fb
kxw <kxw@janesi.com>
2019-09-25 16:32:14 +0800

    home

---
+++ src/pages/home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="home">{{data}}</div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  data() {
+    return {
+      data: ""
+    };
+  },
+  mounted() {
+    this.getData();
+  },
+  methods: {
+    getData() {
+      this.data = "成功";
+    }
+  }
+};
+</script>
+
+<style>
+</style>
